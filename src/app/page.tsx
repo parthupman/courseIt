9e7cc414f2f53d8dd74d79780c2f775f0e2086ca
parthupman <parthupman@gmail.com>
2024-02-14 00:12:25 +0530

    setup shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-red-600">home</h1>
+    <Button>Hello</Button>
   );
 }
